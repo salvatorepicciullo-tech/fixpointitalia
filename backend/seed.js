9b8c01277ad9c02595c37d894d39abac2aebcfdb
Salvatore Picciullo <salvatore.picciullo@gmail.com>
2026-02-25 12:37:59 +0100

    fix seed db path railway

--- backend/seed.js
+++ backend/seed.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, 'fixpoint.db');
+const dbPath = path.join(__dirname, '..', 'fixpoint.db');
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
