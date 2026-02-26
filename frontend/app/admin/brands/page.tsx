cf269aa50e750e236d23054b8fc6741c4bc30e2a
Salvatore Picciullo <salvatore.picciullo@gmail.com>
2026-02-26 23:25:53 +0100

    models definitivo no reflash

--- frontend/app/admin/brands/page.tsx
+++ frontend/app/admin/brands/page.tsx
@@ -1,6 +1,5 @@
 'use client';
-export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-no-store';
+
 
 import { apiFetch } from '@/lib/api';
 import { useEffect, useState } from 'react';
